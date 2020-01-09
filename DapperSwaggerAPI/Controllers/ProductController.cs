b1434f9051ef16334dd058ea026d4e90c88fcc45
dashtharindu <dashtharindu@gmail.com>
2020-01-09 23:40:06 +0530

    Add project files.

---
+++ DapperSwaggerAPI/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DapperSwaggerAPI.Data;
+using DapperSwaggerAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperSwaggerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepository productRepository;
+        public ProductController()
+        {
+            productRepository = new ProductRepository();
+        }
+
+        [HttpGet]
+        public ActionResult<Product> Get()
+        {
+            return Ok(productRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return productRepository.GetById(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromForm]Product prod)
+        {
+            if (ModelState.IsValid)
+            {
+                productRepository.Add(prod);
+                return Ok("Successfully Added");
+            }
+            else
+            {
+                return Ok("Not a valid model");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromForm]Product prod)
+        {
+            prod.ProductId = id;
+            if (ModelState.IsValid)
+            {
+                return Ok(productRepository.Update(prod));
+            }
+            return Ok("Update Failed");
+        }
+
+        //todo-do not pass id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(productRepository.Delete(id));
+        }
+    }
+}
---
+++ DapperSwaggerAPI/Data/ProductRepository.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using DapperSwaggerAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperSwaggerAPI.Data
+{
+    public class ProductRepository
+    {
+        private string ConnectionString;
+        public ProductRepository()
+        {
+            ConnectionString = @"Server=.;Database=TestData;Trusted_Connection=true";
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(ConnectionString);
+            }
+        }
+
+        public void Add(Product prod)
+        {
+            using(IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"INSERT INTO Products (Name,Quantity,Price) VALUES (@Name,@Quantity,@Price)";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, prod);
+            }
+        }
+
+        public List<Product> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT * FROM Products";
+                dbConnection.Open();
+                return dbConnection.Query<Product>(sQuery).ToList();
+            }
+        }
+
+        public Product GetById(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT * FROM Products WHERE ProductId=@Id";
+                dbConnection.Open();
+                return dbConnection.Query<Product>(sQuery, new { Id = id }).FirstOrDefault();
+            }
+        }
+
+        public string Delete(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"DELETE FROM Products WHERE ProductId=@Id";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, new { Id = id });
+                return "Sucesssfully Deleted";
+            }
+        }
+
+        public string Update(Product prod)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"UPDATE Products SET Name=@Name,Quantity=@Quantity,Price=@Price WHERE ProductId=@ProductId";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, prod);
+                //Tim said execute sould work cuz no return data like insert,delete update need execute command.
+                //to get data back in return you need Query command
+                return "Updated";
+            }
+        }
+    }
+}
